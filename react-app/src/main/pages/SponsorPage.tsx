065ddfbd30ca14add801af2dd506d3814d3cc359
WillHCode <williamh@hotmail.be>
2024-07-13 23:50:01 +0200

    Merge package.json

--- react-app/src/main/pages/SponsorPage.tsx
+++ react-app/src/main/pages/SponsorPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import {useNavigate, useParams} from 'react-router-dom';
 import { getDatabase, ref, get } from "firebase/database";
 import Navbar from "../component/Navbar.tsx";
